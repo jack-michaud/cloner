c41366c3a7d545d6fbc65afe87727c27b04ce885
Jack Michaud <jack@lomz.me>
2022-09-16 21:44:04 -0400

    format

--- flake.nix
+++ flake.nix
@@ -8,9 +8,16 @@
       };
     };
 
-    defaultPackage.x86_64-linux = import ./default.nix {
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
+    defaultPackage = {
+      aarch64-linux = import ./default.nix {
+        pkgs = import nixpkgs {
+          system = "aarch64-linux";
+        };
+      };
+      x86_64-linux = import ./default.nix {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+        };
       };
     };
 
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
-use std::{env, fs};
+use git_url_parse::GitUrl;
 use std::process::Command;
 use std::vec::Vec;
-use git_url_parse::GitUrl;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if (args.len() < 2) {
         println!("No git url provided");
-        return
+        return;
     };
 
     let url = &args[1];
     println!("Got url: {}", url);
 
     if let Ok(git_url) = GitUrl::parse(url.as_str()) {
-        let target_dir = format!("{}/Code/{}/{}", env::var("HOME").unwrap(), git_url.host.unwrap(), git_url.owner.unwrap());
+        let target_dir = format!(
+            "{}/Code/{}/{}",
+            env::var("HOME").unwrap(),
+            git_url.host.unwrap(),
+            git_url.owner.unwrap()
+        );
         println!("Creating target: {}", target_dir);
         fs::create_dir_all(target_dir.clone()).unwrap();
         let command = Command::new("git")
@@ -23,7 +28,6 @@ fn main() {
             .arg(format!("{}/{}", target_dir, git_url.name))
             .output()
             .expect("Unable to clone url");
-        println!("{:?}", command);
         println!("Cloned into: {}", git_url.name);
     } else {
         eprintln!("Unable to parse url");
